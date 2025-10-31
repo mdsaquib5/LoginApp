e68e329d8943192c0d161eeae8d12f2713574e0d
mdsaquib5 <mdsaquib5540@gmail.com>
2025-10-31 17:51:11 +0530

    Check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <>
         <div className="min-h-screen flex justify-center items-center w-full gap-4">
-          <Link href="/login1" className="text-blue-700 bg-white/80">Login 1</Link>
-          <Link href="/login2" className="text-blue-700 bg-white/80">Login 2</Link>
+          <Link href="/login1" className="text-blue-700 bg-white/80">Login Page 1</Link>
+          <Link href="/login2" className="text-blue-700 bg-white/80">Login Page 2</Link>
         </div>
     </>
   );
